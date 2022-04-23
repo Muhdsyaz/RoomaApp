dfb114c0738b08583a591cb56cfae1a1adab6a76
Syaz <zanie455@gmail.com>
2022-04-24 07:32:07 +0800

    -Designing the interface of home layout

--- app/src/main/java/my/edu/utem/ftmk/bitp3453/rooma/HomeActivity.java
+++ app/src/main/java/my/edu/utem/ftmk/bitp3453/rooma/HomeActivity.java
@@ -3,18 +3,260 @@ package my.edu.utem.ftmk.bitp3453.rooma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    TextView tvLocation, tvPrice, tvClear;
+    Spinner spResType, spMinPrice, spMaxPrice, spState, spCity;
+    Button btCancel, btApply, btCancel2, btApply2, btSearch;
+
+    String category, minPrice, maxPrice, state, city, location;
+
+    LinearLayout layoutPrice, layoutLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //declare spinner
+        spResType = findViewById(R.id.spResType);
+        spMinPrice = findViewById(R.id.spMinPrice);
+        spMaxPrice = findViewById(R.id.spMaxPrice);
+        spState = findViewById(R.id.spState);
+        spCity = findViewById(R.id.spCity);
+
+        //declare textview
+        tvLocation = findViewById(R.id.tvLocation);
+        tvPrice = findViewById(R.id.tvPrice);
+        tvClear = findViewById(R.id.tvClear);
+
+        //declare button
+        btCancel = findViewById(R.id.btCancel);
+        btApply = findViewById(R.id.btApply);
+        btCancel2 = findViewById(R.id.btCancel2);
+        btApply2 = findViewById(R.id.btApply2);
+        btSearch = findViewById(R.id.btSearch);
+
+        //declare layout
+        layoutPrice = findViewById(R.id.layoutPrice);
+        layoutLocation = findViewById(R.id.layoutLocation);
+
+        //get initial value from textview
+        location = tvLocation.getText().toString();
+
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.category, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spResType.setAdapter(adapter1);
+        spResType.setOnItemSelectedListener(this);
+        category = spResType.getSelectedItem().toString();
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.minprice, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spMinPrice.setAdapter(adapter2);
+        spMinPrice.setOnItemSelectedListener(this);
+        minPrice = spMinPrice.getSelectedItem().toString();
+
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.maxprice, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spMaxPrice.setAdapter(adapter3);
+        spMaxPrice.setOnItemSelectedListener(this);
+        maxPrice = spMaxPrice.getSelectedItem().toString();
+
+
+
+        tvPrice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutPrice.setVisibility(View.VISIBLE);
+
+                btCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        layoutPrice.setVisibility(View.INVISIBLE);
+                    }
+                });
+
+                btApply.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(!minPrice.equals("Any") && !maxPrice.equals("Any")) {
+                            tvPrice.setText(minPrice + " - " + maxPrice);
+
+                            layoutPrice.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+
+            }
+        });
+
+        tvLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutLocation.setVisibility(View.VISIBLE);
+
+                btCancel2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        layoutLocation.setVisibility(View.INVISIBLE);
+                    }
+                });
+
+                btApply2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //set new value to tvLocation
+                        tvLocation.setText(state + " > " + city);
+                        layoutLocation.setVisibility(View.INVISIBLE);
+                    }
+                });
+
+            }
+        });
+
+        tvClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                category = "";
+                minPrice = "";
+                maxPrice = "";
+                state = "";
+                city = "";
+                location = "";
+
+                tvPrice.setText("Price");
+                tvLocation.setText("Location");
+            }
+        });
+
+        // <---- Spinner City and State
+
+        spState = findViewById(R.id.spState);
+        spCity = findViewById(R.id.spCity);
+
+        MalaysiaStateAndCity malaysiaStateCity = new MalaysiaStateAndCity();
+        ArrayList<ArrayList<String>> stateAndCity = malaysiaStateCity.getStateAndCity();
+
+        final ArrayAdapter<String> adapterStateCity = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,stateAndCity.get(0)){
+
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+
+        adapterStateCity.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spState.setAdapter(adapterStateCity);
+        spState.setAdapter(adapterStateCity);
+        spState.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ArrayAdapter adapterCity = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, stateAndCity.get(i)) {
+
+                    @Override
+                    public boolean isEnabled(int position) {
+                        if (position == 0) {
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+
+                    @Override
+                    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                        View view = super.getDropDownView(position, convertView, parent);
+
+                        TextView tv = (TextView) view;
+                        if (position == 0) {
+                            // Set the hint text color gray
+                            tv.setTextColor(Color.GRAY);
+                        } else {
+                            tv.setTextColor(Color.BLACK);
+                        }
+                        return view;
+                    }
+                };
+                spCity.setAdapter(adapterCity);
+
+                String selectedItemText = (String) adapterView.getItemAtPosition(i);
+                if (i > 0) {
+                    // Notify the selected item text
+                    Toast.makeText(getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                    state = spState.getSelectedItem().toString();
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        spCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position > 0) {
+                    String selectedcity = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(getApplicationContext(), "Selected city : " + selectedcity, Toast.LENGTH_SHORT)
+                            .show();
+
+                    city = spCity.getSelectedItem().toString();
+
+                    Log.e("State ", "onCreate: " + state);
+                    Log.e("City ", "onCreate: " + city);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        // -----> Spinner City and State
     }
 
     public void toFavorite(View v){
@@ -38,4 +280,16 @@ public class HomeActivity extends AppCompatActivity {
         moveTaskToBack(true);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        category = spResType.getSelectedItem().toString();
+        minPrice = spMinPrice.getSelectedItem().toString();
+        maxPrice = spMaxPrice.getSelectedItem().toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
